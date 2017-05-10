05c14f990fc5f296ff51385e1479dd048fff48ef
BirskiySergey <birskiy.serg@gmail.com>
2017-05-11 01:56:49 +0300

    type

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,12 @@
 // https://www.npmjs.com/package/gulp-include
 
 // $(function() {})
+
+$('.section-carusel').slick({
+  fade:true,
+  dots: true,
+  arrows:false,
+  infinite: true,
+  // autoplay: true,
+  // autoplaySpeed:3000,
+});
