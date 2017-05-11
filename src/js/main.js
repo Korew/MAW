f086f12812ea47e3aa8bf97de94408ba5fad3fc6
Roman Korenchuk <ikorew@gmail.com>
2017-05-11 19:54:49 +0300

    fixes

--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,27 @@
 // https://www.npmjs.com/package/gulp-include
+//=require jquery/dist/jquery.js
+//=require slick-carousel/slick/slick.js
 
-<<<<<<< HEAD
 // $(function() {})
 
-$('.section-carusel').slick({
-  fade:true,
-  dots: true,
-  arrows:false,
-  infinite: true,
-  // autoplay: true,
-  // autoplaySpeed:3000,
-});
-=======
-//=require jquery/dist/jquery.js
+
 
 
 $(function(){
 
- $('.toggler').click(function(e){
- e.preventDefault();
+	$('.toggler').click(function(e){
+		e.preventDefault();
 
-	var target = $(this).attr('href');
-	$(target).toggleClass('collapse');
-});
+		var target = $(this).attr('href');
+		$(target).toggleClass('show');
+	});
 
-
+	$('.section-carusel').slick({
+		fade:true,
+		dots: true,
+		arrows:false,
+		infinite: true,
+	  // autoplay: true,
+	  // autoplaySpeed:3000,
+	});
 })
->>>>>>> 25620964466fe5493bc03835c8acf62f49626c8a
